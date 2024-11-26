7a9a2e66f21f9641b8038b4c9e3ae4dbbf80b8cb
M R <mreaney03@qub.ac.uk>
2024-11-26 16:54:10 +0000

    Reformatted inflammation-analysis.py updated test_models.py

--- inflammation-analysis.py
+++ inflammation-analysis.py
@@ -2,8 +2,6 @@
 """Software for managing and analysing patients' inflammation data in our imaginary hospital."""
 
 import argparse
-from matplotlib import pyplot as plt
-import numpy as np
 
 from inflammation import models, views
 
@@ -19,13 +17,14 @@ def main(args):
     if not isinstance(InFiles, list):
         InFiles = [args.infiles]
 
-
     for filename in InFiles:
         inflammation_data = models.load_csv(filename)
 
-        view_data = {'average': models.daily_mean(inflammation_data), 'max': models.daily_max(inflammation_data), 'min': models.daily_min(inflammation_data)}
+        view_data = {'average': models.daily_mean(inflammation_data), 'max': models.daily_max(inflammation_data),
+                     'min': models.daily_min(inflammation_data)}
 
         views.visualize(view_data)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
