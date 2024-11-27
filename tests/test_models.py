71781a1d541d2d3331dd7134213e50a799209229
M R <mreaney03@qub.ac.uk>
2024-11-27 16:00:36 +0000

    changes to patient_normalise

--- tests/test_models.py
+++ tests/test_models.py
@@ -4,8 +4,7 @@ import numpy as np
 import numpy.testing as npt
 import pytest
 
-from inflammation.models import daily_max, daily_mean, daily_min
-
+from inflammation.models import daily_max, daily_mean, daily_min, patient_normalise
 @pytest.mark.parametrize(
     "test, expected",
     [
@@ -44,5 +43,40 @@ def test_daily_min(test, expected):
 def test_daily_min_string():
     """Test for TypeError when passing strings"""
 
+
+@pytest.mark.parametrize(
+    "test, expected",
+    [
+        (
+            [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
+            [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
+        ),
+        (
+            [[1, 1, 1], [1, 1, 1], [1, 1, 1]],
+            [[1, 1, 1], [1, 1, 1], [1, 1, 1]],
+        ),
+        (
+            [[float('nan'), 1, 1], [1, 1, 1], [1, 1, 1]],
+            [[0, 1, 1], [1, 1, 1], [1, 1, 1]],
+        ),
+        (
+            [[1, 2, 3], [4, 5, float('nan')], [7, 8, 9]],
+            [[0.33, 0.67, 1], [0.8, 1, 0], [0.78, 0.89, 1]],
+        ),
+        (
+            [[-1, 2, 3], [4, 5, 6], [7, 8, 9]],
+            [[0, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]],
+        ),
+        (
+            [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
+            [[0.33, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]],
+        )
+    ])
+
+def test_patient_normalise(test, expected):
+    """Test normalisation works for arrays of one and positive integers."""
+
+    result = patient_normalise(np.array(test))
+    npt.assert_allclose(result, np.array(expected), rtol=1e-2, atol=1e-2)
     with pytest.raises(TypeError):
         error_expected = daily_min([['Hello', 'there'], ['General', 'Kenobi']])
